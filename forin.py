0f2ed85a02f0617d91eb5616b33c25c9ebe01316
v_vbllv <v_vbllv@tencent.com>
2020-05-21 21:03:43 +0800

    2020-05-21学习补充

--- forin.py
+++ forin.py
@@ -1,27 +1,43 @@
-names = ['Michael', 'Bob', 'Tracy']
-for name in names:
-    print(name)
+# names = ['Michael', 'Bob', 'Tracy']
+# for name in names:
+#     print(name)
 
-total = 0
-for x in [1,2,3,4,5]:
-    total = total + x
-print(total)
+# total = 0
+# for x in [1,2,3,4,5]:
+#     total = total + x
+# print(total)
 
-sum = 0
-for x in range(101):
-    sum = sum + x
-print(sum)
+# sum = 0
+# for x in range(101):
+#     sum = sum + x
+# print(sum)
 
-n = 0
-for x in [1,2,3,4,5]:
-  if x == 3:
-    break
-  n = n + x
-print(n)
+# n = 0
+# for x in [1,2,3,4,5]:
+#   if x == 3:
+#     break
+#   n = n + x
+# print(n)
 
-w = 0
-for x in [1,2,3,4,5]:
-  if x == 3:
-    continue
-  w = w + x
-print(w)
+# w = 0
+# for x in [1,2,3,4,5]:
+#   if x == 3:
+#     continue
+#   w = w + x
+# print(w)
+
+
+# 迭代对象
+# d = {'a': 'a1', 'b': 'b1', 'c': 'c1'}
+# for key in d :
+#   print(key, d[key])
+
+# 迭代数组list
+l = ['a', 'b', 'c', 'd']
+for key in l:
+  print(key)
+
+# 迭代string
+# s = 'ABCD'
+# for k in s:
+#   print(k)
